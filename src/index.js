8d3690ed47c850914399dc1fab90f7a4ee65722d
sushantp432 <sushantpagare55@gmail.com>
2022-07-24 02:34:42 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+// import { store } from './store/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
+ReactDOM.render(
+  
   <React.StrictMode>
-    
-     <BrowserRouter>
-        <ChakraProvider>
-            <App />
-        </ChakraProvider>
+    <BrowserRouter>
+    <App />
     </BrowserRouter>
-
-  </React.StrictMode>
-  </>
+    </React.StrictMode>,
+  
+  
+       document.getElementById('root')
 );
 
 reportWebVitals();
